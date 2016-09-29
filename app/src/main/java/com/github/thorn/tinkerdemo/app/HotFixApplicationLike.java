118dbcd44709d3bd65eb178d1388190d84993907
ostea <cnostea@iCloud.com>
2016-09-29 16:24:27 +0800

    init

---
+++ app/src/main/java/com/github/thorn/tinkerdemo/app/HotFixApplicationLike.java
@@ -0,0 +1,45 @@
+package com.github.thorn.tinkerdemo.app;
+
+import android.annotation.TargetApi;
+import android.app.Application;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.content.res.Resources;
+import android.os.Build;
+import android.support.multidex.MultiDex;
+
+import com.github.thorn.tinkerdemo.util.HotFixApplicationContext;
+import com.tencent.tinker.anno.DefaultLifeCycle;
+import com.tencent.tinker.loader.app.DefaultApplicationLike;
+import com.tencent.tinker.loader.shareutil.ShareConstants;
+
+/**
+ * Desc:
+ * Created by Thorn on 16/9/29.
+ */
+
+@DefaultLifeCycle(
+        application = "com.github.thorn.tinkerdemo.app.HotFixApplication",
+        flags = ShareConstants.TINKER_ENABLE_ALL,
+        loadVerifyFlag = false
+)
+public class HotFixApplicationLike extends DefaultApplicationLike {
+
+    public HotFixApplicationLike(Application application, int tinkerFlags, boolean tinkerLoadVerifyFlag, long applicationStartElapsedTime, long applicationStartMillisTime, Intent tinkerResultIntent, Resources[] resources, ClassLoader[] classLoader, AssetManager[] assetManager) {
+        super(application, tinkerFlags, tinkerLoadVerifyFlag, applicationStartElapsedTime, applicationStartMillisTime, tinkerResultIntent, resources, classLoader, assetManager);
+    }
+
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+    @Override
+    public void onBaseContextAttached(Context base) {
+        super.onBaseContextAttached(base);
+        //you must install multiDex whatever tinker is installed!
+        MultiDex.install(base);
+
+        HotFixApplicationContext.application = getApplication();
+        HotFixApplicationContext.context = getApplication();
+
+
+    }
+}
---
+++ app/src/main/java/com/github/thorn/tinkerdemo/util/HotFixApplicationContext.java
@@ -0,0 +1,13 @@
+package com.github.thorn.tinkerdemo.util;
+
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * Desc:
+ * Created by Thorn on 16/9/29.
+ */
+public class HotFixApplicationContext {
+    public static Application application = null;
+    public static Context context = null;
+}
